821c9db74da054ae30d9df9f504e456c58ea7507
vanshuhassija <33016642+vanshuhassija@users.noreply.github.com>
2022-08-16 22:18:42 +0530

    Counter Example State And Styling

--- Counter.js
+++ Counter.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+//Linking CSS Files
+//import <path to file>.css
+import "./index.css";
 class App extends React.Component {
   // A way where we can maintain the variables and it automitically
   //updates the UI when Changed
@@ -22,30 +24,28 @@ class App extends React.Component {
   //Interpolation ---> Interpolate the value
 
   render() {
+    const incrementCounter = () => {
+      //Increment the counter variable
+      // this.state.counter++;
+      //This is not allowed in React
+      //State is immutable in React
+      this.setState({
+        //Pass only those keys that you need to change
+        counter: this.state.counter + 1,
+      });
+      //Pass an Object To Set State
+      // Whenever a state variable changes, the nodes that are dependent
+      //on that variable, get updated.
+    };
+    //Style needs to be Js Object Always
     return (
       <div>
         {/* Show the current counter variable */}
         {/* Whenever you want to write, some Js/dynamic, wrap it in {} */}
         {/* Anything that should be computed */}
-        <h1>{this.state.counter}</h1>
+        <h1 className="counter">{this.state.counter}</h1>
         {/* Try to make arrow functions everytime */}
-        <button
-          onClick={() => {
-            //Increment the counter variable
-            // this.state.counter++;
-            //This is not allowed in React
-            //State is immutable in React
-            this.setState({
-              //Pass only those keys that you need to change
-              counter: this.state.counter + 1,
-            });
-            //Pass an Object To Set State
-            // Whenever a state variable changes, the nodes that are dependent 
-            //on that variable, get updated.
-          }}
-        >
-          Increment
-        </button>
+        <button onClick={incrementCounter}>Increment</button>
       </div>
     );
   }
